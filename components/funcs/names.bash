80e8703dfd5538aed8f5181fb9c9a73a11d82b6e
Ken Dait <snaginanek@gmail.com>
2019-04-16 09:40:05 -0500

    added functionality to source functions from indiviual modules

---
+++ components/funcs/names.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# function: names
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    names ()
+    {
+        __list_out_computer_names ()
+        {
+            local -A names
+            names=(['LocalHostName']='' ['HostName']='' ['ComputerName']='')
+            for key in "${!names[@]}";do
+                names[$key]="$(scutil --get "${key}" 2> /dev/null)"
+                if [[ -z "${names[$key]}" ]]; then
+                    names[$key]="< NOT SET >"
+                fi
+            done
+            for key in "${!names[@]}"; do
+                echo -e "${key}: ${names[$key]}"
+            done \
+                | awk -F": " '{printf ("%*s\t%s\n"),-20,$1,$2}' \
+                | sed -E -e '1i\
+                \
+                \	COMPUTER NAMES:\
+                \
+                ' -e '$a\
+                \
+                ' -e 's:^:	:'
+            return
+        }
+
+        if (($#==0)); then
+            __list_out_computer_names
+        fi
+        return
+    }
+fi
