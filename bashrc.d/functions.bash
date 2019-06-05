d41869aed0c7947e91cfe73c60358bc72f51d86f
Ken Dait <snaginanek@gmail.com>
2019-06-05 02:23:33 -0500

    moved each separate declared function to module file

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -3,8 +3,45 @@
 
 #echo "FILE SOURCED: ${BASH_SOURCE[0]}"
 
-hello ()
+__findFunctionModuleFiles ()
 {
-    echo "HELLO ${1:-WORLD}" \!
+    find "${funcs:?}" \
+        -mindepth 1 -maxdepth 1 \
+        -type f \
+        \! \( \
+            -name "*.swp" \
+            -or -name "_*" \
+            -or -name ".*" \
+        \)
     return
 }
+
+__reportLoadedFunctions ()
+{
+    __printHeading "FUNCS (${#loadedFuncs[@]}):"
+    for f in "${loadedFuncs[@]}"; do
+        echo -e "\\t- ${f}"
+    done
+    echo
+    return
+}
+
+if [[ -d ~/.bashrc.d/function_modules ]]; then
+    if [[ -z "${funcs:-}" ]]; then
+        declare -rx funcs=~/.bashrc.d/function_modules
+    fi
+
+    (( ${#loadedFuncs[@]} > 0 )) && unset loadedFuncs
+
+    declare -ax loadedFuncs=()
+
+    mapfile loadedFuncs < <(__findFunctionModuleFiles)
+    #__reportLoadedFunctions
+
+    if (( ${#loadedFuncs[@]} > 0 )); then
+        for f in "${loadedFuncs[@]}"; do
+            . ${f}
+        done
+    fi
+    
+fi
