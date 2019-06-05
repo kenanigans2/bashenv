87e56cf56af2a0d96690f002f4d1bc8ec111cf06
Ken Dait <snaginanek@gmail.com>
2019-06-05 07:21:34 -0500

    refactor aliases: alias mods

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -6,3 +6,28 @@
 alias x='exit'
 alias c='clear'
 
+if [[ -d ~/.bashrc.d/alias_modules ]]; then
+
+    #
+    #-:SET VARS
+    {
+        [[ -z "${aliasMods:-}" ]] && declare -x aliasMods=~/.bashrc.d/alias_modules
+        [[ -n "${loadedAliasMods[*]}" ]] && unset loadedAliasMods
+        declare -xa loadedAliasMods
+        loadedAliasMods=()
+    }
+    
+    #
+    #-:SET ALIAS MOD FILE ARRAY
+    mapfile loadedAliasMods < <(find ~/.bashrc.d/alias_modules -type f \! \
+        \( -name ".*" -or -name "*.swp" -or -name "_*" \) \
+        | grep -v '^[[:blank:]]*$')
+
+    #
+    #-:SOURCE EACH ALIAS MOD
+    for a in "${loadedAliasMods[@]}"; do
+        a="$(echo "$a" | tr -d '\n')"
+        . "${a}"
+    done
+    echo
+fi
